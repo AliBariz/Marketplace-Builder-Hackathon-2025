d461d9707ef20fe9a034f31304ea1bc5297fc822
Ali Bariz <160329277+AliBariz@users.noreply.github.com>
2025-02-07 21:36:23 +0500

    Update featuredProducts.tsx

--- app/components/Products/featuredProducts.tsx
+++ app/components/Products/featuredProducts.tsx
@@ -2,7 +2,6 @@
 import React from "react";
 import Image from "next/image";
 import { ShoppingCart } from "lucide-react";
-import { Product } from "@/types/products";
 
 const FeaturedProducts = () => {
   return (
